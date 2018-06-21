c4abc38d50b03c82778a279ff183f28ce980a5e5
Fouter <1420126933@qq.com>
2018-06-22 00:04:43 +0800

    finish the create of product

--- src/com/neuedu/model/po/Product.java
+++ src/com/neuedu/model/po/Product.java
@@ -5,5 +5,93 @@ public class Product {
 	private String product_name;
 	private FirstCategory fc;
 	private SecondCategory sc;
+	private String measure;
+	private double original_price;
+	private double discount;
+	private double cost_price;
+	private String version;
+	private Supplier supplier;
+	private String publisher;
+	private String shelf_life;
+	private String remarks;
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public FirstCategory getFc() {
+		return fc;
+	}
+	public void setFc(FirstCategory fc) {
+		this.fc = fc;
+	}
+	public SecondCategory getSc() {
+		return sc;
+	}
+	public void setSc(SecondCategory sc) {
+		this.sc = sc;
+	}
+	public String getMeasure() {
+		return measure;
+	}
+	public void setMeasure(String measure) {
+		this.measure = measure;
+	}
+	public double getOriginal_price() {
+		return original_price;
+	}
+	public void setOriginal_price(double original_price) {
+		this.original_price = original_price;
+	}
+	public double getDiscount() {
+		return discount;
+	}
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+	public double getCost_price() {
+		return cost_price;
+	}
+	public void setCost_price(double cost_price) {
+		this.cost_price = cost_price;
+	}
+	public String getVersion() {
+		return version;
+	}
+	public void setVersion(String version) {
+		this.version = version;
+	}
+	public Supplier getSupplier() {
+		return supplier;
+	}
+	public void setSupplier(Supplier supplier) {
+		this.supplier = supplier;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public String getShelf_life() {
+		return shelf_life;
+	}
+	public void setShelf_life(String shelf_life) {
+		this.shelf_life = shelf_life;
+	}
+	public String getRemarks() {
+		return remarks;
+	}
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+	
 	
 }
